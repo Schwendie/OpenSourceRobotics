d62dfc851f1837e079728b52248c30edb902fbf8
Kyle Schwendiman <kyleschwendiman@gmail.com>
2018-08-27 18:17:17 -0400

    Continued working on assignment_5

--- assignment_5.py
+++ assignment_5.py
@@ -7,8 +7,26 @@ from math import cos, sin, pi
 
 plt.ion()
 
-w = 1
+x0 = 0
+y0 = 0
+w = 1 # width of robot
 dt = 0.01
 
 x_traj = []
-x = 0
+theta = 0
+
+for vel in velocities:
+    omg = vel[0] - vel[1]
+    v = omg / 2
+    theta = theta + omg*dt
+
+if __name__ == "__main__":
+
+    velocities = []
+
+    with open("wheel_velocities.txt", "r") as file:
+        for line in file:
+            data = line.rstrip().split(' ')
+            data[0] = float(data[0])
+            data[1] = float(data[1])
+            velocities.append(data)

28c419d87f7e52fd99515feeb0edcbdcc26f3116
Kyle Schwendiman <kyleschwendiman@gmail.com>
2018-08-29 13:05:20 -0400

    adding assignment files

---
+++ lab2/assignment_1.py
@@ -0,0 +1,12 @@
+class Vehicle:
+    def __init__(self, x=0, y=0, theta=0, length=1, width=0.5):
+        fig = plot.figure()
+        fig.canvas.mpl_connect('button_press_event', self.click)
+
+    def update_pose():
+
+    def transformation_matrix():
+
+    def plot():
+
+    def click():

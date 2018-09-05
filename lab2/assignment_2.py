01eb6e2fca5c129e6f8bdc7a9aa5fdf7bdaab06e
Kyle Schwendiman <kyleschwendiman@gmail.com>
2018-09-05 09:44:41 -0400

    Finished assignment_2

--- lab2/assignment_2.py
+++ lab2/assignment_2.py
@@ -12,25 +12,29 @@ v = 0
 v_x = 0
 v_y = 0
 theta = 0
+theta_goal = 0
+omg = 0
 K_p = 1
+K_h = 1
 dt = 0.01
 ve = Vehicle()
 
+def ang_diff(theta1, theta2):
+    return (theta1 - theta2 + pi)%(2*pi) - pi
 
 while True:
-	x_goal = ve.goal[0]
-	y_goal = ve.goal[1]
-	dx = x_goal - x
-	dy = y_goal - y
-	d = sqrt(dx**2 + dy**2)
-	v = K_p * d
-	theta = atan2(dy, dx)
-	v_x = v * cos(theta)
-	v_y = v * sin(theta)
-	x = x + v_x*dt
-	y = y + v_y*dt
+    x_goal = ve.goal[0]
+    y_goal = ve.goal[1]
+    dx = x_goal - x
+    dy = y_goal - y
+    d = sqrt(dx**2 + dy**2)
+    v = K_p * d
+    theta_goal = atan2(dy, dx)
+    v_x = v * cos(theta)
+    v_y = v * sin(theta)
+    x = x + v_x*dt
+    y = y + v_y*dt
+    omg = K_h * ang_diff(theta_goal, theta)
+    theta = theta + omg*dt
 
-	ve.update_pose(x, y, theta)
-
-def ang_diff(theta1, theta2):
-	return (theta1 - theta2 + pi)%(2*pi) - pi
+    ve.update_pose(x, y, theta)
